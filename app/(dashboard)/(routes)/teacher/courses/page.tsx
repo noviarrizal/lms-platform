35a50fa80ada0852cbc5a99cc90de59c85d1f86e
arifnoviarrizal <noviarrizalarif@gmail.com>
2023-09-19 18:12:32 +0700

    feat: adding create course page and react hot toast

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 const CoursesPage = () => {
-  return <div>CoursesPage</div>;
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default CoursesPage;
